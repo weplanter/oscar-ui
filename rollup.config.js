cd8418a45b920b20f5a4c0b3b5073435d59bc8b9
zrain <zrainma@sina.com>
2019-04-18 23:23:52 +0800

    # build less

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,44 @@ import resolve from 'rollup-plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
 import postcss from 'rollup-plugin-postcss';
 import svg from 'rollup-plugin-react-svg';
+// import less from 'less';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
+
+// const processLess = function(context, payload) {
+//     return new Promise((resolve, reject) => {
+//         less.render(
+//             {
+//                 file: context
+//             },
+//             function(err, result) {
+//                 if (!err) {
+//                     resolve(result);
+//                 } else {
+//                     reject(err);
+//                 }
+//             }
+//         );
+
+//         less.render(context, {}).then(
+//             function(output) {
+//                 // output.css = string of css
+//                 // output.map = string of sourcemap
+//                 // output.imports = array of string filenames of the imports referenced
+//                 if (output && output.css) {
+//                     resolve(output.css);
+//                 } else {
+//                     reject({});
+//                 }
+//             },
+//             function(err) {
+//                 reject(err);
+//             }
+//         );
+//     });
+// };
 
 export default {
     input: `./src/index.js`,
@@ -14,11 +48,13 @@ export default {
     output: [
         {
             file: `./dist/es/index.js`,
+            // css: `./dist/es/style/index.css`,
             format: 'es',
             name: '@oscarui'
         },
         {
             file: `./dist/umd/index.js`,
+            // css: `./dist/es/style/index.css`,
             format: 'umd',
             name: '@oscarui'
         }
@@ -27,10 +63,10 @@ export default {
         resolve(),
         filesize(),
         postcss({
-            plugins: [autoprefixer, cssnano],
+            plugin: [autoprefixer, cssnano],
             extract: './dist/style/index.css',
-            minimize: isProductionEnv,
-            user: ['less']
+            minimize: isProductionEnv
+            // process: processLess
         }),
         babel({
             exclude: 'node_modules/**',
